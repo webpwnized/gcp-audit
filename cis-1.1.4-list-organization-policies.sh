3a405c212f2fe60e3e58cc1cc00fe7e21e1f5c0e
webpwnized <webpwnized@gmail.com>
2022-01-24 19:08:32 -0500

    0.0.1 Added CIS 1.1 scripts

---
+++ cis-1.1.4-list-organization-policies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+declare ORGANIZATION_IDS=$(gcloud organizations list --format="flattened(ID)" | grep id | cut -d " " -f 2 | cut -d "/" -f 2)
+
+for ORGANIZATION_ID in $ORGANIZATION_IDS; do
+	echo "Organization Policy for Organization $ORGANIZATION_IDS"
+	echo ""
+	gcloud resource-manager org-policies list --organization=$ORGANIZATION_ID;
+	echo ""
+done;
--- utility-list-organization-admins.sh
+++ utility-list-organization-admins.sh
@@ -3,8 +3,8 @@
 declare ORGANIZATION_IDS=$(gcloud organizations list --format="flattened(ID)" | grep id | cut -d " " -f 2 | cut -d "/" -f 2)
 
 for ORGANIZATION_ID in $ORGANIZATION_IDS; do
-	echo "IAM Policy for Project $ORGANIZATION_IDS"
+	echo "Organization Admins for $ORGANIZATION_IDS"
 	echo ""
-	gcloud organizations get-iam-policy $ORGANIZATION_ID;
+	gcloud organizations get-iam-policy $ORGANIZATION_ID | grep -B 1 'roles/resourcemanager.organizationAdmin' | grep 'user' | cut -d : -f2;
 	echo ""
 done;
