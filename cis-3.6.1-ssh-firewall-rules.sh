33e4b1fb95a080e0b5f162f2f51eb13020ac7716
webpwnized <webpwnized@gmail.com>
2022-03-31 17:00:53 -0400

    1.0.2 Updated firewall rule scanner

--- cis-3.6.1-ssh-firewall-rules.sh
+++ cis-3.6.1-ssh-firewall-rules.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 declare PROJECT_IDS=$(gcloud projects list --format="flattened(PROJECT_ID)" | grep project_id | cut -d " " -f 2);
+#declare PROJECT_IDS="gcp-dsi-uat";
+
+declare SEPARATOR="----------------------------------------------------------------------------------------";
 
 for PROJECT_ID in $PROJECT_IDS; do
 
@@ -15,10 +18,14 @@ for PROJECT_ID in $PROJECT_IDS; do
 		PROJECT_APPLICATION=$(echo $PROJECT_DETAILS | jq '.labels.app');
 		PROJECT_OWNER=$(echo $PROJECT_DETAILS | jq '.labels.adid');
 
+		echo $SEPARATOR;
 		echo "Firewall rules for project $PROJECT_ID";
 		echo "";
 		
 		echo $RESULTS | jq -rc '.[]' | while IFS='' read FIREWALL_RULE;do
+
+			ALLOWED_LABEL="";
+			DENIED_LABEL="";
 		
 			NAME=$(echo $FIREWALL_RULE | jq '.name');
 			ALLOWED=$(echo $FIREWALL_RULE | jq -c '.allowed');
@@ -30,8 +37,15 @@ for PROJECT_ID in $PROJECT_IDS; do
 			DEST_RANGES=$(echo $FIREWALL_RULE | jq -c '.destinationRanges');
 			DEST_TAGS=$(echo $FIREWALL_RULE | jq -c '.sourceTags');
 			DISABLED=$(echo $FIREWALL_RULE | jq '.disabled');
+			HAS_INTERNET_SOURCE=$(echo $SOURCE_RANGES | jq '.[]' | jq 'select(. | contains("0.0.0.0/0"))');
+			ALLOWS_SSH=$(echo $ALLOWED | jq 'map(.ports)' | jq '.[] | index("22")');
+			ALLOWS_RDP=$(echo $ALLOWED | jq 'map(.ports)' | jq '.[] | index("3389")');
+			ALLOWS_HTTP=$(echo $ALLOWED | jq 'map(.ports)' | jq '.[] | index("80")');
 			
-			echo "Name: $NAME ($DIRECTION)";
+			if [[ $ALLOWED != "null" ]]; then ALLOWED_LABEL="ALLOWED"; fi;
+			if [[ $DENIED != "null" ]]; then DENIED_LABEL="DENIED"; fi;
+
+			echo "Name: $NAME ($DIRECTION $ALLOWED_LABEL$DENIED_LABEL)";
 			echo "Project Name: $PROJECT_NAME";
 			echo "Project Application: $PROJECT_APPLICATION";
 			echo "Project Owner: $PROJECT_OWNER";
@@ -43,10 +57,17 @@ for PROJECT_ID in $PROJECT_IDS; do
 			if [[ $DEST_TAGS != "null" ]]; then echo "Destination Tags: $DEST_TAGS"; fi;
 			if [[ $LOG_CONFIG != "null" ]]; then echo "Logging: $LOG_CONFIG"; fi;
 			if [[ $DISABLED != "null" ]]; then echo "Disabled: $DISABLED"; fi;
-			
+			if [[ $ALLOWED_LABEL != "" && $HAS_INTERNET_SOURCE != "" ]]; then echo "Violation: Allows acccess from entire Internet"; fi;
+			if [[ "$ALLOWS_SSH" =~ ^[0-9]+$ ]]; then echo "Violation: Rule includes port 22/SSH"; fi;
+			if [[ "$ALLOWS_RDP" =~ ^[0-9]+$ ]]; then echo "Violation: Rule includes port 3389/RDP"; fi;
+			if [[ "$ALLOWS_HTTP" =~ ^[0-9]+$ ]]; then echo "Violation: Rule includes port 80/HTTP"; fi;
+			if [[ $NAME == "default-allow-icmp" ]]; then echo "Violation: Default ICMP rule implemented"; fi;
+			if [[ $NAME == "default-allow-ssh" ]]; then echo "Violation: Default SSH rule implemented"; fi;
+			if [[ $NAME == "default-allow-rdp" ]]; then echo "Violation: Default RDP rule implemented"; fi;			
 			echo "";
 		done;
 	else
+		echo $SEPARATOR;
 		echo "No firewall rules found for $PROJECT_ID";
 		echo "";
 	fi;
