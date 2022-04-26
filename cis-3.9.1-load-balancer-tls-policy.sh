0d50b8ae205f375f60a458d9b84e443ffc9364b5
webpwnized <webpwnized@gmail.com>
2022-04-25 20:01:43 -0400

    1.0.5 Added cis 3.9.1

---
+++ cis-3.9.1-load-balancer-tls-policy.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+declare PROJECT_IDS=$(gcloud projects list --format="flattened(PROJECT_ID)" | grep project_id | cut -d " " -f 2);
+declare PROJECT_IDS="gcp-xolt-cla10-prod";
+
+declare SEPARATOR="----------------------------------------------------------------------------------------";
+
+for PROJECT_ID in $PROJECT_IDS; do
+
+	gcloud config set project $PROJECT_ID;
+	
+	PROJECT_DETAILS=$(gcloud projects describe $PROJECT_ID --format="json");
+	PROJECT_NAME=$(echo $PROJECT_DETAILS | jq -rc '.name');
+	PROJECT_APPLICATION=$(echo $PROJECT_DETAILS | jq -rc '.labels.app');
+	PROJECT_OWNER=$(echo $PROJECT_DETAILS | jq -rc '.labels.adid');
+
+	echo "";
+	echo $SEPARATOR;
+	echo "Project Name: $PROJECT_NAME";
+	echo "Project Application: $PROJECT_APPLICATION";
+	echo "Project Owner: $PROJECT_OWNER";
+	echo "";
+
+	declare RESULTS=$(gcloud compute target-http-proxies list --quiet --format="json");
+
+	if [[ $RESULTS != "[]" ]]; then
+		
+		echo $SEPARATOR;
+		echo "Insecure HTTP Load Balancers for project $PROJECT_ID";
+		echo $SEPARATOR;
+		echo "";
+		
+		echo $RESULTS | jq -r -c '.[]' | while IFS='' read -r HTTP_PROXY;do
+			NAME=$(echo $HTTP_PROXY | jq -rc '.name');
+			echo "Name: $NAME (Insecure)";
+		done;
+		echo "";
+	else
+		echo $SEPARATOR;
+		echo "No HTTP Load Balancers found for $PROJECT_ID";
+		echo "";
+	fi;
+	
+	declare RESULTS=$(gcloud compute target-tcp-proxies list --quiet --format="json");
+
+	if [[ $RESULTS != "[]" ]]; then
+		
+		echo $SEPARATOR;
+		echo "Insecure TCP Load Balancers for project $PROJECT_ID";
+		echo $SEPARATOR;
+		echo "";
+		
+		echo $RESULTS | jq -r -c '.[]' | while IFS='' read -r TCP_PROXY;do
+			NAME=$(echo $TCP_PROXY | jq -rc '.name');
+			echo "Name: $NAME (Insecure)";
+		done;
+		echo "";
+	else
+		echo $SEPARATOR;
+		echo "No TCP Load Balancers found for $PROJECT_ID";
+		echo "";
+	fi;
+
+	declare RESULTS=$(gcloud compute target-ssl-proxies list --quiet --format="json");
+
+	if [[ $RESULTS != "[]" ]]; then
+		
+		echo $SEPARATOR;
+		echo "TLS Load Balancers for project $PROJECT_ID";
+		echo $SEPARATOR;
+		echo "";
+		
+		echo $RESULTS | jq -r -c '.[]' | while IFS='' read -r SSL_PROXY;do
+			NAME=$(echo $SSL_PROXY | jq -rc '.name');
+			echo "Name: $NAME";
+		done;
+		echo "";
+	else
+		echo $SEPARATOR;
+		echo "No TLS Load Balancers found for $PROJECT_ID";
+		echo "";
+	fi;
+
+	declare RESULTS=$(gcloud compute target-https-proxies list --quiet --format="json");
+
+	if [[ $RESULTS != "[]" ]]; then
+	
+		echo $RESULTS | jq '.';
+		
+		echo $SEPARATOR;
+		echo "HTTPS Load Balancers for project $PROJECT_ID";
+		echo $SEPARATOR;
+		echo "";
+		
+		echo $RESULTS | jq -r -c '.[]' | while IFS='' read -r HTTPS_PROXY;do
+			NAME=$(echo $HTTPS_PROXY | jq -rc '.name');
+			SSL_POLICY=$(echo $HTTPS_PROXY | jq -rc '.sslPolicy');
+			
+			echo "Name: $NAME $DEFAULT_POLICY_VIOLATION";
+			if [[ $SSL_POLICY == "null" ]]; then echo "Violation: Using default TLS policy"; fi;
+			echo "";
+		done;
+		echo "";
+	else
+		echo $SEPARATOR;
+		echo "No HTTPS Load Balancers found for $PROJECT_ID";
+		echo "";
+	fi;
+	
+	sleep 1;
+
+done;
+
