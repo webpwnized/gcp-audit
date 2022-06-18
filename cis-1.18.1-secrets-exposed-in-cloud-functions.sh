bd42cf837a026d56f368640869416a2c5bb2f290
webpwnized <webpwnized@gmail.com>
2022-06-17 20:46:58 -0400

    1.0.11 Add cis-1.16.1-list-organization-essential-contacts.sh cis-1.18.1-secrets-exposed-in-cloud-functions.sh

---
+++ cis-1.18.1-secrets-exposed-in-cloud-functions.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+declare PROJECT_IDS=$(gcloud projects list --format="flattened(PROJECT_ID)" | grep project_id | cut -d " " -f 2);
+
+declare SEPARATOR="----------------------------------------------------------------------------------------";
+
+for PROJECT_ID in $PROJECT_IDS; do
+
+	gcloud config set project $PROJECT_ID;
+
+	declare RESULTS=$(gcloud functions list --quiet --format="json");
+
+	if [[ $RESULTS != "[]" ]]; then
+		
+		PROJECT_DETAILS=$(gcloud projects describe $PROJECT_ID --format="json");
+		PROJECT_NAME=$(echo $PROJECT_DETAILS | jq -rc '.name');
+		PROJECT_APPLICATION=$(echo $PROJECT_DETAILS | jq -rc '.labels.app');
+		PROJECT_OWNER=$(echo $PROJECT_DETAILS | jq -rc '.labels.adid');
+
+		echo $SEPARATOR;
+		echo "Cloud functions for project $PROJECT_ID";
+		echo "Project Name: $PROJECT_NAME";
+		echo "Project Application: $PROJECT_APPLICATION";
+		echo "Project Owner: $PROJECT_OWNER";
+		echo "";
+		
+		echo $RESULTS | jq -r -c '.[]' | while IFS='' read -r CLOUD_FUNCTION;do
+		
+			NAME=$(echo $CLOUD_FUNCTION | jq -rc '.name' | cut -d '/' -f 6);
+			BUILD_ENIVRONMENT_VARIABLES=$(echo $CLOUD_FUNCTION | jq '.buildEnvironmentVariables');
+			ENIVRONMENT_VARIABLES=$(echo $CLOUD_FUNCTION | jq '.environmentVariables');
+			INGRESS_SETTINGS=$(echo $CLOUD_FUNCTION | jq '.ingressSettings');
+
+			echo "Name: $NAME";
+			echo "Build Environment Variables: $BUILD_ENIVRONMENT_VARIABLES";
+			echo "Environment Variables: $ENIVRONMENT_VARIABLES";
+			if [[ $INGRESS_SETTINGS =~ "ALLOW_ALL" ]]; then echo "Violation: Cloud function allows all ingress"; fi;
+			echo "";
+		done;
+	else
+		echo $SEPARATOR;
+		echo "No cloud functions found for $PROJECT_ID";
+		echo "";
+	fi;
+	sleep 0.5;
+done;
+
