0e259e77c8d7c8968e767d47c9f8b517bc822d4e
webpwnized <webpwnized@gmail.com>
2022-06-20 19:59:20 -0400

    1.0.13 Updated cis-1.1.4-list-organization-policies.sh

--- cis-1.1.4-list-organization-policies.sh
+++ cis-1.1.4-list-organization-policies.sh
@@ -5,6 +5,6 @@ declare ORGANIZATION_IDS=$(gcloud organizations list --format="flattened(ID)" | 
 for ORGANIZATION_ID in $ORGANIZATION_IDS; do
 	echo "Organization Policy for Organization $ORGANIZATION_IDS"
 	echo ""
-	gcloud resource-manager org-policies list --organization=$ORGANIZATION_ID;
+	gcloud resource-manager org-policies list --organization=$ORGANIZATION_ID | grep "SET";
 	echo ""
 done;
