132bb271b3990739c46e4af87da9a6b24fe3792f
webpwnized <webpwnized@gmail.com>
2022-07-20 17:17:04 -0400

    1.0.26 Added utility-list-cloud-run-services.sh

--- utility-list-cloud-run-services.sh
+++ utility-list-cloud-run-services.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-declare PROJECT_IDS=$(gcloud projects list --format="flattened(PROJECT_ID)" | grep project_id | cut -d " " -f 2)
+LONG=project:
+SHORT=p:
+OPTS=$(getopt -a -n testscript --options $SHORT --longoptions $LONG -- "$@")
+
+eval set -- "$OPTS"
+while :
+do
+    case "$1" in --project | -p )
+        declare PROJECT_IDS="$2"
+        shift 2
+     ;;
+     -- )
+        shift;
+        break
+        ;;
+        *)
+        exit 2
+    esac
+done;
+
+if [[ $PROJECT_IDS == "" ]]; then
+    declare PROJECT_IDS=$(gcloud projects list --format="flattened(PROJECT_ID)" | grep project_id | cut -d " " -f 2);
+fi;
 
 for PROJECT_ID in $PROJECT_IDS; do	
 	gcloud config set project $PROJECT_ID;
@@ -14,12 +36,14 @@ for PROJECT_ID in $PROJECT_IDS; do
 
 		echo $SERVICES | jq -rc '.[]' | while IFS='' read -r SERVICE;do
 		
-			echo $SERVICE | jq -rc '.';
-		
-			NAME=$(echo $SERVICE | jq -rc '.name');
+			NAME=$(echo $SERVICE | jq -rc '.metadata.name');
+			INGRESS_SETTING=$(echo $SERVICE | jq -rc '.metadata.annotations."run.googleapis.com/ingress"');
+			
+			echo "Service Name: $NAME";
+			echo "Ingress Setting: $INGRESS_SETTING";
 						
-			if [[ $NAME != "" ]]; then
-				echo "SERVICE Name: $NAME";
+			if [[ $INGRESS_SETTING == "all" ]]; then
+				echo "Violation: The ingress setting is configured to ALL, which allows all requests including requests directly from the internet";
 			fi;
 			echo "";
 		done;
