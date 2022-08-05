85c6886d5f5bf604dae1b5f4d69eadeab635adb1
webpwnized <webpwnized@gmail.com>
2022-08-05 10:59:39 -0400

    1.0.30 Fixed utility-list-accounts-with-privilege.sh

--- utility-list-accounts-with-privilege.sh
+++ utility-list-accounts-with-privilege.sh
@@ -10,7 +10,6 @@ declare PROJECTS=$(gcloud projects list --format="json");
 
 if [[ $PROJECTS != "[]" ]]; then
 
-<<<<<<< HEAD
     echo $PROJECTS | jq -rc '.[]' | while IFS='' read PROJECT;do
 
         PROJECT_NAME=$(echo $PROJECT | jq -r '.name');
@@ -26,21 +25,6 @@ if [[ $PROJECTS != "[]" ]]; then
             echo "";
         fi;
     done;
-=======
-		PROJECT_NAME=$(echo $PROJECT | jq -r '.name');
-		PROJECT_OWNER=$(echo $PROJECT | jq -r '.labels.adid');
-		PROJECT_APPLICATION=$(echo $PROJECT | jq -r '.labels.app');
-		MEMBERS=$(gcloud projects get-iam-policy $PROJECT_NAME --format="json" | jq -r '.bindings[] | 	select(.role=="roles/'$ROLE'") | .members[]');
-
-		if [[ $MEMBERS != "" ]]; then
-			echo "Project Name: $PROJECT_NAME";
-			echo "Project Owner: $PROJECT_OWNER";
-			echo "Project Application: $PROJECT_APPLICATION";			
-			echo -e "Members ($ROLE role):\n$MEMBERS";
-			echo "";
-		fi;
-	done;
->>>>>>> 08a7cdb1ead61c70c5c8e3951289633e280716e6
 else
     echo "No projects found";
     echo "";
