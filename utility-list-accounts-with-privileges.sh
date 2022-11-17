7e6dc87f46edbf17cddda2d61baec5279812d1cf
webpwnized <webpwnized@gmail.com>
2022-11-17 18:04:42 -0500

    1.0.43 Fixed bug in utility-list-accounts-with-privileges.sh

--- utility-list-accounts-with-privileges.sh
+++ utility-list-accounts-with-privileges.sh
@@ -5,7 +5,7 @@ declare PROJECT_IDS="";
 declare DEBUG="False";
 declare CSV="False";
 declare HELP=$(cat << EOL
-	$0 [-r,--role] [-p, --project PROJECT] [-d, --debug] [-h, --help]	
+	$0 [-r,--role] [-p, --project PROJECT] [--csv] [-d, --debug] [-h, --help]	
 EOL
 );
 
@@ -47,23 +47,34 @@ fi;
 
 if [[ $PROJECT_IDS != "[]" ]]; then
     echo $PROJECT_IDS | jq -rc '.[]' | while IFS='' read PROJECT;do
+
+	PROJECT_ID=$(echo $PROJECT | jq -r '.projectId');
         PROJECT_NAME=$(echo $PROJECT | jq -r '.name');
         PROJECT_OWNER=$(echo $PROJECT | jq -r '.labels.adid');
         PROJECT_APPLICATION=$(echo $PROJECT | jq -r '.labels.app');
-        MEMBERS=$(gcloud projects get-iam-policy $PROJECT_NAME --format="json" | jq -r '.bindings[] |     select(.role=="roles/'$ROLE'") | .members[]');
+        MEMBERS=$(gcloud projects get-iam-policy $PROJECT_ID --format="json" | jq -r '.bindings[] | select(.role=="roles/'$ROLE'") | .members[]');
+        ENVIRONMENT="";
+        
+        for ENV in "sandbox" "dev" "sys" "uat" "prod"; do
+		if [[ $(grep -ic $ENV <<< $PROJECT_ID) == 1 ]]; then
+			ENVIRONMENT=$ENV;
+		fi;
+	done;
 
         if [[ $MEMBERS != "" ]]; then
         	if [[ $CSV != "True" ]]; then
+        		echo "Project ID: $PROJECT_ID";
 			echo "Project Name: $PROJECT_NAME";
 			echo "Project Owner: $PROJECT_OWNER";
 			echo "Project Application: $PROJECT_APPLICATION";            
 			echo -e "Members ($ROLE role):\n$MEMBERS";
+			echo "Environment: $ENVIRONMENT";
             		echo "";
         	else
         		for MEMBER in $MEMBERS;do
         			ACCOUNT_TYPE=$(echo $MEMBER | cut -d ":" -f1);
         			ACCOUNT=$(echo $MEMBER | cut -d ":" -f2);
-        			echo "$PROJECT_NAME, $PROJECT_OWNER, $PROJECT_APPLICATION, $ACCOUNT, $ACCOUNT_TYPE";
+        			echo "$PROJECT_ID, \"$PROJECT_NAME\", $PROJECT_OWNER, $PROJECT_APPLICATION, $ACCOUNT, $ACCOUNT_TYPE, $ENVIRONMENT";
         		done;
         	fi;
         fi;
