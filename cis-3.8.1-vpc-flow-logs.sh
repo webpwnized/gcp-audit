4a8cc49c1f586b0804011846bd7e9869f2cb3371
ProGritz <pat07082@gmail.com>
2023-04-12 17:07:03 -0400

    Renamed helpers.inc file to functions.inc and corrected syntax errors for --include-column-headers option

--- cis-3.8.1-vpc-flow-logs.sh
+++ cis-3.8.1-vpc-flow-logs.sh
@@ -7,7 +7,7 @@ declare DEBUG="False";
 declare CSV="False";
 declare ICH="False";
 declare HELP=$(cat << EOL
-	$0 [-p, --project PROJECT] [--csv] [-d, --debug] [-h, --help]	
+	$0 [-p, --project PROJECT] [-c, --csv] [-i, --include-column-headers] [-d, --debug] [-h, --help]	
 EOL
 );
 
@@ -17,7 +17,7 @@ for arg in "$@"; do
     "--help") 			set -- "$@" "-h" ;;
     "--debug") 			set -- "$@" "-d" ;;
     "--csv") 			set -- "$@" "-c" ;;
-    "--include-column-headers") set -- "$@" "-ich" ;;
+    "--include-column-headers") set -- "$@" "-i" ;;
     "--project")   		set -- "$@" "-p" ;;
     *)        			set -- "$@" "$arg"
   esac
@@ -33,7 +33,7 @@ do
         	DEBUG="True";;
         c)
         	CSV="True";;
-	ich)	
+	i)	
 		ICH="True";;
         h)
         	echo $HELP; 
