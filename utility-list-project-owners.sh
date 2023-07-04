a6d0359db37414ea3be191dfe461e57edcd9af90
Mohammad Hussain <msh52@njit.edu>
2023-07-04 19:12:06 -0400

    adding options for help, debug, and CSV to the script

--- utility-list-project-owners.sh
+++ utility-list-project-owners.sh
@@ -1,16 +1,56 @@
 #!/bin/bash
 
+source functions.inc
+
 declare RESULTS=$(gcloud projects list --format="json");
 
+declare DEBUG="False";
+declare CSV="False";
+declare HELP=$(cat << EOL
+	$0 [-c, --csv] [-d, --debug] [-h, --help]	
+EOL
+);
+
+for arg in "$@"; do
+  shift
+  case "$arg" in
+    "--help") 	set -- "$@" "-h" ;;
+    "--debug") 	set -- "$@" "-d" ;;
+    "--csv") 	set -- "$@" "-c" ;;
+    *)        	set -- "$@" "$arg"
+  esac
+done
+
+while getopts "hdcp:" option
+do 
+    case "${option}"
+        in
+        d)
+        	DEBUG="True";;
+        c)
+        	CSV="True";;
+        h)
+        	echo $HELP; 
+        	exit 0;;
+    esac;
+done;
+
 if [[ $RESULTS != "[]" ]]; then
+	if [[ $CSV == "True" ]]; then
+		echo "\"PROJECT_NAME\",\"PROJECT_APPLICATION\",\"PROJECT_OWNER\"";
+	fi
 		
-	echo $RESULTS | jq -rc '.[]' | while IFS='' read PROJECT;do
+	echo $RESULTS | jq -rc '.[]' | while IFS='' read PROJECT; do
 
 		NAME=$(echo $PROJECT | jq -rc '.name');
 		APPLICATION=$(echo $PROJECT | jq -rc '.labels.app');
 		OWNER=$(echo $PROJECT | jq -rc '.labels.adid');
-		
-		echo "$NAME ($APPLICATION): $OWNER";
+
+		if [[ $CSV == "True" ]]; then
+			echo "\"$NAME\",\"$APPLICATION\",\"$OWNER\"";
+		else
+			echo "$NAME ($APPLICATION): $OWNER";
+		fi;
 	done;
 else
 	echo "No projects found";
