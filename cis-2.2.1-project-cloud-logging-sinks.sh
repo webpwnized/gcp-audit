cdd837623f69652d05bab958e4e217e8f7592b85
Hussain185 <98345252+Hussain185@users.noreply.github.com>
2023-07-04 04:50:19 -0400

    Update cis-2.2.1-project-cloud-logging-sinks.sh

--- cis-2.2.1-project-cloud-logging-sinks.sh
+++ cis-2.2.1-project-cloud-logging-sinks.sh
@@ -8,7 +8,7 @@ declare DEBUG="False";
 declare CSV="False";
 declare ICH="False";
 declare HELP=$(cat << EOL
-	$0 [-p, --project PROJECT] [-c, --csv] [-i, --include-column-headers] [-d, --debug] [-h, --help]	
+	$0 [-p, --project PROJECT] [-c, --csv] [-d, --debug] [-h, --help]	
 EOL
 );
 
@@ -18,7 +18,6 @@ for arg in "$@"; do
     "--help") 			set -- "$@" "-h" ;;
     "--debug") 			set -- "$@" "-d" ;;
     "--csv") 			set -- "$@" "-c" ;;
-    "--include-column-headers") set -- "$@" "-i" ;;
     "--project")   		set -- "$@" "-p" ;;
     *)        			set -- "$@" "$arg"
   esac
@@ -34,8 +33,6 @@ do
         	DEBUG="True";;
         c)
         	CSV="True";;
-	i) 	
-		ICH="True";;
         h)
         	echo $HELP; 
         	exit 0;;
@@ -61,7 +58,7 @@ if [[ $DEBUG == "True" ]]; then
 	echo "";
 fi;
 
-if [[ $ICH == "True" ]]; then
+if [[ $CSV == "True" ]]; then
 	echo "\"PROJECT_ID\", \"PROJECT_NAME\", \"PROJECT_OWNER\", \"PROJECT_APPLICATION\", \"SINK_NAME\", \"SINK_DESTINATION\", \"SINK_FILTER_IS_DEFAULT_DEFAULT\", \"SINK_FILTER_IS_REQUIRED_DEFAULT\", \"SINK_FILTER_MESSAGE\", \"SINK_FILTER\"";	
 fi;
 
