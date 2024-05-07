b7486ab0fc84d00b85198d6310b190a3d1c6e2d0
webpwnized <webpwnized@gmail.com>
2024-05-06 22:08:51 -0400

    1.1.1 Updated utilities

--- src/utility-list-project-ids.sh
+++ src/utility-list-project-ids.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
-gcloud projects list --format="flattened(PROJECT_ID)" | grep project_id | cut -d " " -f 2
+# Function to display help menu
+display_help() {
+    cat << EOF
+Usage: $0 [options]
+
+Description:
+  This script retrieves a list of project IDs from Google Cloud Platform using
+  the 'gcloud' command-line tool.
+
+Options:
+  -h, --help    Display this help menu
+EOF
+    exit 0
+}
+
+# Parse command line options
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h|--help)
+            display_help
+            ;;
+        *)
+            echo "Error: Unknown option '$1'"
+            display_help
+            ;;
+    esac
+done
+
+# Retrieve list of project IDs from gcloud
+gcloud projects list --format="value(PROJECT_ID)"
--- src/utility-list-projects.sh
+++ src/utility-list-projects.sh
@@ -1,23 +1,68 @@
 #!/bin/bash
 
-declare RESULTS=$(gcloud projects list --format="json");
+################################################################################
+# Script Name: utility-list-projects.sh
+# Description: This script retrieves a list of projects from Google Cloud
+#              Platform using the 'gcloud' command-line tool and displays
+#              relevant details for each project, such as name, application,
+#              and owner. It also supports a delay between project outputs.
+# Usage:       ./utility-list-projects.sh [options]
+# Options:     -h, --help    Display this help menu
+################################################################################
 
+# Include common constants
+source ./common-constants.inc
+
+# Function to display help menu
+display_help() {
+    cat << EOF
+Usage: $0 [options]
+
+Description:
+  This script retrieves a list of projects from Google Cloud Platform using the
+  'gcloud' command-line tool and displays relevant details for each project,
+  such as name, application, and owner. It also supports a delay between
+  project outputs.
+
+Options:
+  -h, --help     Display this help menu
+EOF
+    exit 0
+}
+
+# Parse command line options
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h|--help)
+            display_help
+            ;;
+        *)
+            echo "Error: Unknown option '$1'"
+            display_help
+            ;;
+    esac
+done
+
+# Get project list from gcloud
+RESULTS=$(gcloud projects list --format="json")
+
+# Check if there are any projects
 if [[ $RESULTS != "[]" ]]; then
-		
-	echo $RESULTS | jq -rc '.[]' | while IFS='' read PROJECT;do
+    # Process each project
+    while IFS= read -r PROJECT; do
+        # Extract project details
+        NAME=$(jq -r '.name' <<< "$PROJECT")
+        APPLICATION=$(jq -r '.labels.app' <<< "$PROJECT")
+        OWNER=$(jq -r '.labels.adid' <<< "$PROJECT")
 
-		NAME=$(echo $PROJECT | jq '.name');
-		APPLICATION=$(echo $PROJECT | jq '.labels.app');
-		OWNER=$(echo $PROJECT | jq '.labels.adid');
-		
-		echo "Name: $NAME";
-		if [[ $APPLICATION != "null" ]]; then echo "Application: $APPLICATION"; fi;		
-		if [[ $OWNER != "null" ]]; then echo "Owner: $OWNER"; fi;
-		echo $BLANK_LINE;
-		sleep $SLEEP_SECONDS;
-	done;
+        # Output project details
+        echo "Name: $NAME"
+        [[ $APPLICATION != "null" ]] && echo "Application: $APPLICATION"
+        [[ $OWNER != "null" ]] && echo "Owner: $OWNER"
+        echo "" # Blank line between projects
+        sleep "$SLEEP_SECONDS" # Introduce delay between projects
+    done < <(jq -rc '.[]' <<< "$RESULTS")
 else
-	echo "No projects found";
-	echo $BLANK_LINE;
-fi;
-
+    # No projects found
+    echo "No projects found"
+fi
